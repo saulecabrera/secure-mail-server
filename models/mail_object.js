5eab2e68348b9dd634a0ed64a78fbe7896f6c3f4
saulecabrera <saulecabrera@gmail.com>
2014-09-10 20:24:43 -0600

    started MailObject schema

---
+++ models/mail_object.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
+var timestamps = require('mongoose-timestamp');
+var User = require('./user');
+
+var MailObjectSchema = new Schema({
+	
+});
