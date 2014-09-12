550cf698eed3ce5ff071eef48fcabc66d11ec9a5
saulecabrera <saulecabrera@gmail.com>
2014-09-12 17:35:24 -0600

    signup page .25 complete

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', layout:'main_layout'});
+  res.render('index', { title: 'secure mail server', layout:'main_layout'});
+});
+
+router.get('/signup', function(req, res){
+	res.render('signup', {title: 'secure mail server - signup'})
 });
 
 module.exports = router;
